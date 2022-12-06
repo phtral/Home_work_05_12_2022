24287682f140327c624af5b7a4dfe753753612ed
Philip Trubitsyn <trph@bk.ru>
2022-12-06 13:03:01 +0300

    Третья задача из домашнего задания

---
+++ 3d/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число N ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; n >= i; i++)
+{
+    int result = (int)(Math.Pow(i,3));
+    Console.WriteLine($"Куб числа {i} равен {result}");
+}
